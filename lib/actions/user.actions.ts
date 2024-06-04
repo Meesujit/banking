0cdc3266ff32388b8ce594124b512839afc64ac3
Meesujit <sujit.gda997@gmail.com>
2024-06-04 13:41:17 +0530

    Todo plaid

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,10 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 
 import { parseStringify } from "../utils";
+import { CountryCode, Products } from "plaid";
+
+import { plaidClient } from "@/lib/plaid";
+
 
 export const signIn = async ({ email, password }: signInProps) => {
   try {
@@ -64,5 +68,27 @@ export const logOutAccount = async () => {
     await account.deleteSession('current');
   } catch (error) {
     return null;
+  }
+}
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+        client_name: user.name,
+
+      },
+      client_name : user.name,
+      products: ['auth'] as Products[],
+      language: 'en',
+      country_codes: ['US'] as CountryCode[],
+
+    }
+
+    const response  = await plaidClient.linkTokenCreate(tokenParams);
+    return parseStringify({linkToken: response.data.link_token})
+  } catch (error) {
+    console.log(error); 
   }
 }
